7e66681d75827de2b8791665914850d76d8ff573
Taylor, Gerard1 <gerard1.taylor@intel.com>
2019-02-01 12:12:36 -0800

    KMEANS Clustering Implementation

---
+++ KMEANS/include/kmeans.hpp
@@ -0,0 +1,79 @@
+#ifndef __KMEANS_HPP
+#define __KMEANS_HPP
+#include "common.hpp"
+#include <unordered_set>
+#include <limits>
+#include <vector>
+#include <cstdlib>
+#include <cmath>
+#include <map>
+typedef struct cluster 
+{
+ std::vector<double> *centroid;
+ std::vector<data *> *cluster_points;
+ std::map<int, int> class_counts;
+ int most_frequent_class;
+ cluster(data *initial_point)
+ {
+   centroid = new std::vector<double>;
+   cluster_points = new std::vector<data *>;
+   for(auto val : *(initial_point->get_feature_vector()))
+   {
+     centroid->push_back(val);
+   }
+   cluster_points->push_back(initial_point);
+   class_counts[initial_point->get_label()] = 1;
+   most_frequent_class = initial_point->get_label();
+ }
+ 
+void add_to_cluster(data* point)
+ {
+   int previous_size = cluster_points->size();
+   cluster_points->push_back(point);
+   for(int i = 0; i < centroid->size(); i++)
+   {
+   	double val = centroid->at(i);
+     val *= previous_size;
+     val += point->get_feature_vector()->at(i);
+     val /= (double)cluster_points->size();
+     centroid->at(i) = val;
+   }
+   if(class_counts.find(point->get_label()) == class_counts.end())
+   {
+     class_counts[point->get_label()] = 1;
+   } else
+   {
+     class_counts[point->get_label()]++;
+   }
+ }
+ void set_most_frequent_class()
+ {
+   int best_class;
+   int freq = 0;
+   for(auto kv : class_counts)
+   {
+    if(kv.second > freq)
+    {
+      freq = kv.second;
+      best_class = kv.first;
+    }
+   }
+   most_frequent_class = best_class;
+ }
+} cluster_t;
+
+class kmeans : public common_data
+{
+  int num_clusters;
+  std::vector<cluster_t *> *clusters;
+  std::unordered_set<int> *used_indexes;
+  public:
+  kmeans(int k);
+  void init_clusters();
+  void init_clusters_for_each_class();
+  void train();
+  double euclidean_distance(std::vector<double> *, data *);
+  double validate();
+  double test();
+};
+#endif
---
+++ KMEANS/src/kmeans.cc
@@ -0,0 +1,148 @@
+#include "../include/kmeans.hpp"
+#include "data_handler.h"
+
+kmeans::kmeans(int k)
+{
+ num_clusters = k;
+ clusters = new std::vector<cluster_t *>;
+ used_indexes = new std::unordered_set<int>;
+}
+
+void kmeans::init_clusters()
+{
+ for(int i = 0; i < num_clusters; i++)
+ {
+   int index = (rand() % training_data->size());
+   while(used_indexes->find(index) != used_indexes->end())
+   {
+     index = (rand() % training_data->size());
+   }
+   clusters->push_back(new cluster_t(training_data->at(index)));
+   used_indexes->insert(index);
+ }
+}
+
+void kmeans::init_clusters_for_each_class()
+{
+ std::unordered_set<int> classes_used;
+ for(int i = 0; i < training_data->size(); i++)
+ {
+   if(classes_used.find(training_data->at(i)->get_label()) == classes_used.end())
+   {
+     clusters->push_back(new cluster_t(training_data->at(i)));
+     classes_used.insert(training_data->at(i)->get_label());
+     used_indexes->insert(i);
+   }
+ }
+}
+void kmeans::train()
+{
+ while(used_indexes->size() < training_data->size())
+ {
+   int index = (rand() % training_data->size());
+   while(used_indexes->find(index) != used_indexes->end())
+   {
+     index = (rand() % training_data->size());
+   }
+   double min_dist = std::numeric_limits<double>::max();
+   int best_cluster = 0;
+   for(int j = 0; j < clusters->size(); j++)
+   {
+     double dist = euclidean_distance(clusters->at(j)->centroid, training_data->at(index));
+     if(dist < min_dist)
+     {
+       min_dist = dist;
+       best_cluster = j;
+     }
+   }
+   clusters->at(best_cluster)->add_to_cluster(training_data->at(index));
+   used_indexes->insert(index);
+ }
+}
+
+double kmeans::euclidean_distance(std::vector<double> * centroid, data *query_point)
+{
+ double dist = 0.0;
+ for(int i = 0; i < centroid->size(); i++)
+ {
+   dist += pow(centroid->at(i) - query_point->get_feature_vector()->at(i), 2);
+ }
+ return sqrt(dist);
+}
+double kmeans::validate()
+{
+ double num_correct = 0.0;
+ for(auto query_point : *validation_data)
+ {
+   double min_dist = std::numeric_limits<double>::max();
+   int best = 0;
+   for(int i = 0; i < clusters->size(); i++)
+   {
+     double current_dist = euclidean_distance(clusters->at(i)->centroid, query_point);
+     if(current_dist < min_dist)
+     {
+       min_dist = current_dist;
+       best = i;
+     }
+   }
+   if(clusters->at(best)->most_frequent_class == query_point->get_label()) num_correct++;
+ }
+ return 100.0 * (num_correct / (double) validation_data->size());
+}
+double kmeans::test()
+{
+ double num_correct = 0.0;
+ for(auto query_point : *test_data)
+ {
+   double min_dist = std::numeric_limits<double>::max();
+   int best = 0;
+   for(int i = 0; i < clusters->size(); i++)
+   {
+     double current_dist = euclidean_distance(clusters->at(i)->centroid, query_point);
+     if(current_dist < min_dist)
+     {
+       min_dist = current_dist;
+       best = i;
+     }
+   }
+   if(clusters->at(best)->most_frequent_class == query_point->get_label()) num_correct++;
+ }
+ return 100.0 * (num_correct / (double) test_data->size());
+}
+
+int
+main()
+{
+  data_handler *dh = new data_handler();
+  dh->read_input_data("../train-images-idx3-ubyte");
+  dh->read_label_data("../train-labels-idx1-ubyte");
+  dh->count_classes();
+  dh->split_data();
+  double performance = 0;
+  double best_performance = 0;
+  int best_k = 1;
+  for(int k = dh->get_class_counts(); k < dh->get_training_data()->size()*0.1; k++)
+  {
+    kmeans *km = new kmeans(k);
+    km->set_training_data(dh->get_training_data());
+    km->set_test_data(dh->get_test_data());
+    km->set_validation_data(dh->get_validation_data());
+    km->init_clusters();
+    km->train();
+    performance = km->validate();
+    printf("Current Perforamnce @ K = %d: %.2f\n", k, performance);
+    if(performance > best_performance)
+    {
+      best_performance = performance;
+      best_k = k;
+    }
+  }
+  kmeans *km = new kmeans(best_k);
+  km->set_training_data(dh->get_training_data());
+  km->set_test_data(dh->get_test_data());
+  km->set_validation_data(dh->get_validation_data());
+  km->init_clusters();
+  km->train();
+  printf("Overall Performance: %.2f\n",km->test());
+
+}
